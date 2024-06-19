e9a3bb0c751ba8feb5a236a17f58bde2b9c566b9
PiedraL <joaquinlangone12@gmail.com>
2024-06-19 20:57:54 -0300

    Working and learging about http requests

--- 10-http.js
+++ 10-http.js
@@ -1,10 +1,11 @@
+const fs = require('node:fs')
 const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 0
 
 const processRequest = (req, res) => {
 
-    res.setHeader('Content-Type', 'text/html ; charset=utf-8')
+    res.setHeader('Content-Type', 'text/html ; charset=utf-8') // => We have to tell to the server how to treat the info
 
     if (req.url === '/') {
         res.statusCode = 200
@@ -12,6 +13,18 @@ const processRequest = (req, res) => {
     } else if (req.url === '/contact' ) {
         res.statusCode = 200
         res.end( '<h1>Contacts</h1>' )
+    } else if (req.url === '/beatiful.png') {
+        
+        fs.readFile('./love.png', (err , data) => {
+            if (err) {
+                res.statusCode = 500
+                res.end('<h1>Internal Error - 500</h1>')
+            } else {
+                res.setHeader('Content-Type', 'image/png') // => we've to tell to the server how to treat the info
+                res.end(data)
+            }
+        })
+    
     } else {
         res.statusCode = 404
         res.end( '<h1>404 - Page not found :(</h1>' )
