7e5a02790968dfe6fa78070e75130f699a705dc1
PiedraL <joaquinlangone12@gmail.com>
2024-07-04 01:39:48 -0300

    Working in basic movies back/front app

--- class-3/public/main.js
+++ class-3/public/main.js
@@ -1,1 +1,63 @@
-console.log('Is this working?')
+const genreBtn = document.querySelectorAll('.genre_btn')
+
+
+function fetchMovies (genre = '') {
+
+    let url = 'http://localhost:1234/movies'
+    if (genre) {
+        url += `?genre=${encodeURIComponent(genre)}`
+    } 
+
+    fetch(url)
+        .then(res => res.json())
+        .then(movies => {
+
+            renderMovies(movies)
+
+        })
+        .catch( err => console.err(err))
+}
+
+    
+
+function renderMovies ( movies ) {
+
+    if ( movies.length > 0 ) {
+        const movie = movies.map( movie => {
+            return `
+                <article>
+                    <h2>${movie.title}</h2>
+                    <img src="${movie.poster}" alt="${movie.title}">
+                    <div>${movie.genre}</div>
+                </article>
+
+            `
+        }).join('')
+
+        document.querySelector('#movies').innerHTML = movie
+    } else {
+        console.log('error')
+    }
+
+}
+
+genreBtn.forEach( btn => {
+    btn.addEventListener('click', (event) => {
+        const genre = event.target.name
+        fetchMovies(genre) 
+
+        const newURL = new URL(window.location)
+        newURL.searchParams.set('genre', genre)
+        window.history.pushState({} , '' , newURL)
+    })
+})
+
+// handle back/forward nav
+window.addEventListener('popstate' , () => {
+    const params = new URLSearchParams(window.location.search)
+    const genre = params.get('genre')
+    fetchMovies(genre)
+})
+
+fetchMovies()
+
